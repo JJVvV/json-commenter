f94dfb47006c0935ddd94baf57c4d15235c6f1c6
hzliujunwei <hzliujunwei@corp.netease.com>
2017-10-14 01:31:02 +0800

    init

---
+++ index.js
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+/******************************************
+ *  Author : Author   
+ *  Created On : Thu Oct 12 2017
+ *  File : index.js
+ *******************************************/
+
+let path = require('path')
+let fs = require('fs')
+let program = require('commander')
+let {exec} = require('child_process')
+// 默认文件后缀
+let extension = '.jc'
+// 默认文件输出路径
+let defaultOutput = './'
+let createFolders = (to) => {
+    
+    let sep = path.sep,
+        folder = '',
+        folders
+   
+    folders = path.dirname(to).split(sep)
+    while(folders.length){
+        folder += folders.shift() + sep
+        if(!fs.existsSync(folder)){
+            // exec('')
+            fs.mkdirSync(folder, 0777)
+        }
+    }
+}
+
+function list(val){
+    return val.split(',')
+}
+
+program.version('0.1.0')
+        .usage('json文件')
+        .description('生成json文件')
+
+program.option('-w --watch [value]', '监视', list)
+        .option('-f --file <value>', '入口文件', list)
+        .option('-t --to [value]', '输出文件', defaultOutput)
+        .option('-e --ext [value]', '文件后缀', extension)
+
+    
+program.parse(process.argv)
+
+
+if(program.file){
+    if(Array.isArray(program.file)){
+        program.file.forEach((item)=> {
+            handleFile(item, program.to)
+        })
+    }else{
+        handleFile(program.file, program.to)
+    }
+    
+}
+
+function loopSrc(from, filter=()=>{return true}, callback){
+    
+    if(!fs.existsSync(from)) {
+        console.log(`file not exit: ${from}`)
+        return
+    }
+    if(fs.statSync(from).isDirectory()){
+        
+        let files = fs.readdirSync(from)
+        files.forEach((file, index) => {
+           let curPath = `${from}/${file}`
+         
+           loopSrc(curPath, filter, callback)
+        });
+    }else{
+        
+        if(filter(from)) callback(from)
+    }
+}
+
+function handleFile(fileFrom, fileTo){
+    console.log(fileFrom)
+    // let from = path.join(__dirname, fileFrom)
+    let readWrite = (f, to)=> {
+        fs.readFile(f, (err, data) => {
+            if(err) throw err
+        
+            let content = data.toString()
+            content = (new Function(`let data = ${content}; return data`))()
+            fs.writeFile(to, JSON.stringify(content, null, 4), (err) => {
+                if(err) throw err
+                console.log(`${f}--->${to} 成功`)
+            })
+        })   
+    }
+    loopSrc(fileFrom,(f) => {
+        return path.extname(f) === program.ext
+    }, (f) => {
+       console.log('readwrite')
+        let fromName = path.basename(f, program.ext)
+        let to = path.join(fileTo||'./', `${fromName}.json`)
+        createFolders(to)
+     
+        readWrite(f, to)
+        
+        if(program.watch){
+            console.log(`正在监听${f}`)
+            fs.watchFile(f, () => {
+                console.log('文件变化，正在重新生成')
+                readWrite(f, to)
+            })    
+        }
+    })
+}
+
+program.parse(process.argv)
+
